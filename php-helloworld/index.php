c7386fff1e3f5465d027ba52432eda9844892989
Student User <student@workstation.lab.example.com>
2022-11-07 08:23:05 -0500

    updated-app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "This is the Change for webconsole shit on openshift container!\n";
+print "And another change so lol!\n";
 ?>
